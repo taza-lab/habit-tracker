7ab7d03b8a7c5c5f5bb502a5cdefa9fb0ce13b2c
taza-lab <yanka8m15@gmail.com>
2025-08-17 13:14:28 +0900

    feat: [backend] 日ごとの習慣取得、実行登録API

--- backend/internal/handler/habit_handler.go
+++ backend/internal/handler/habit_handler.go
@@ -10,22 +10,33 @@ import (
 // メモ
 // gin.H = map[string]interface{}
 
+// TODO: モデル作成
 type Habit struct {
-	ID   int    `json:"id"`
+	Id   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type HabitStatus struct {
+    Habit  Habit `json:"habit"`
+    IsDone bool  `json:"isDone"`
+}
+
+type DailyTrack struct {
+	Date   string        `json:"date"`
+	Habits []HabitStatus `json:"habits"`
 }
 
 func GetHabitList(c *gin.Context) {
 	var data = []Habit{
-		{ID: 1, Name: "朝ヨガ"},
-		{ID: 2, Name: "勉強"},
+		{Id: 1, Name: "朝ヨガ"},
+		{Id: 2, Name: "勉強"},
 	}
 
 	c.JSON(http.StatusOK, data)
 }
 
 func GetHabit(c *gin.Context) {
-	var data = Habit{ID: 1, Name: "朝ヨガ"}
+	var data = Habit{Id: 1, Name: "朝ヨガ"}
 
 	c.JSON(http.StatusOK, data)
 }
@@ -39,5 +50,21 @@ func UpdateHabit(c *gin.Context) {
 }
 
 func DeleteHabit(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func GetDailyTrack(c *gin.Context) {
+	var data = DailyTrack{
+		Date: c.Param("date"),
+		Habits: []HabitStatus{
+			{Habit: Habit{Id: 1, Name: "朝ヨガ"}, IsDone: false},
+			{Habit: Habit{Id: 2, Name: "勉強"}, IsDone: false},
+		},
+	}
+
+	c.JSON(http.StatusOK, data)
+}
+
+func UpdateDoneDailyTrack(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
