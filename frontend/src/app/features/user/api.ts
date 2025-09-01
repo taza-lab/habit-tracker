5991c5cae1b479191950ab90cb97f2b15129f18d
taza-lab <yanka8m15@gmail.com>
2025-09-01 09:24:26 +0900

    feat: [frontend] サインアップ機能

--- frontend/src/app/features/user/api.ts
+++ frontend/src/app/features/user/api.ts
@@ -8,10 +8,31 @@ export async function login(username: string, password: string): Promise<{ token
         `${BASE_URL}/login`,
         {
             method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
         }
     );
     if (!res.ok) throw new Error('Failed to login');
+    return res.json();
+}
+
+export async function signup(username: string, password: string, confirmPassword: string): Promise<void> {
+    const res = await fetch(
+        `${BASE_URL}/signup`,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password, confirm_password: confirmPassword })
+        }
+    );
+
+    // ステータスコードが200番台以外の場合
+    if (!res.ok) {
+        const errorData = await res.json();
+        const errorMessage = errorData.error || 'Failed to signup';
+        throw new Error(errorMessage);
+    }
+
     return res.json();
 }
 
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,115 +1,34 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useRouter } from "next/navigation";
-import {
-    Container,
-    Box,
-    TextField,
-    Button,
-    Typography,
-    Alert,
-    CircularProgress,
-    Divider
-} from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { login } from '@/features/user/api';
-import { usePoint } from '@/context/PointContext';
+import { Container } from '@mui/material';
+import LoginBox from "@/features/user/LoginBox"
+import SignUpBox from "@/features/user/SignUpBox"
 
-export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const router = useRouter();
-    const { setPoints } = usePoint();
+export default function AuthPage() {
+    const [success, setSuccess] = useState<string | null>(null);
+    const [showLogin, setShowLogin] = useState(true);
+    const [showSignup, setShowSignup] = useState(false);
 
-    const handleLogin = async () => {
-        setLoading(true);
-        setError(null);
+    // ログインモーダルからサインアップモーダルに切り替える関数
+    const switchToSignup = () => {
+        setSuccess('');
+        setShowLogin(false);
+        setShowSignup(true);
+    };
 
-        try {
-            // API実行
-            const data = await login(username, password);
-
-            // JWTトークンをローカルストレージに保存
-            localStorage.setItem('jwt_token', data.token);
-            localStorage.setItem('username', data.user.username);
-            setPoints(data.user.points);
-
-            // TOP画面リダイレクト
-            router.push("/")
-
-        } catch (err) {
-            setError('Login faild');
-        } finally {
-            setLoading(false);
-        }
+    // サインアップモーダルからログインモーダルに切り替える関数
+    const switchToLogin = () => {
+        setShowSignup(false);
+        setShowLogin(true);
     };
 
     return (
         <Container component="main" maxWidth="xs">
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <LockOutlinedIcon sx={{ fontSize: 40, color: 'primary.main' }} />
-                <Typography component="h1" variant="h5" mt={1}>
-                    Login
-                </Typography>
-                <Box component="form" sx={{ mt: 1, width: '100%' }}>
-                    {/* エラーメッセージ */}
-                    {error && <Alert severity="error" sx={{ my: 2 }}>{error}</Alert>}
-
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="username"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="password"
-                        type="password"
-                        autoComplete="current-password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                        disabled={!username || !password || loading}
-                        onClick={() => handleLogin()}
-                    >
-                        {loading ? <CircularProgress size={24} /> : 'Login'}
-                    </Button>
-
-                    <Divider sx={{ my: 2 }}>or</Divider>
-
-                    <Button
-                        fullWidth
-                        variant="outlined"
-                        sx={{ mt: 3 }}
-                        onClick={() => { router.push("/signup") }}
-                    >
-                        Create Account
-                    </Button>
-                </Box>
-            </Box>
+            {showLogin && !showSignup
+                ? <LoginBox success={success} switchToSignup={switchToSignup} />
+                : <SignUpBox setSuccessMessage={setSuccess} switchToLogin={switchToLogin} />
+            }
         </Container>
     );
 }
